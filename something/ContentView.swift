447151ce28c734f41e8e11e0143d51cec97d7742
Nina Zhou <nzhou@lcs.on.ca>
2023-11-18 19:38:02 -0500

    Initial Commit

---
+++ something/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  something
+//
+//  Created by Shuyu Zhou on 2023-11-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ something/somethingApp.swift
@@ -0,0 +1,17 @@
+//
+//  somethingApp.swift
+//  something
+//
+//  Created by Shuyu Zhou on 2023-11-18.
+//
+
+import SwiftUI
+
+@main
+struct somethingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
